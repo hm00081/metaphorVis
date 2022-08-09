cca4f749782dbf7105c06b5c6e6f38c1d26d6955
hm00081 <hm00081@naver.com>
2022-08-09 14:05:47 +0900

    hi

--- src/components/Navigator/Navigator.tsx
+++ src/components/Navigator/Navigator.tsx
@@ -2,12 +2,13 @@ import styled from 'styled-components';
 import { SankeyData } from '../../types/sankey';
 import { SourceTargetIdLinksDict } from '../Sankey/Sankey';
 import { useState, useEffect } from 'react';
-import Network from './Network/Network.jsx';
+import { Network } from './Network/Network';
 import { PaperView } from './Paperview/PaperView';
+import { fullData } from '../../Data';
 // import { ReactComponent as MyNetwork } from './MyNetwork.svg';
 
 const BigNav = styled.div`
-    display: flex;
+    display: inline-block;
     position: absolute;
     background-color: white;
     width: 750px;
@@ -17,11 +18,12 @@ const BigNav = styled.div`
 `;
 
 function Navigator() {
+    const [originData, setOriginData] = useState<SankeyData>(fullData);
     return (
         <>
             <BigNav>
                 <Network />
-                <PaperView />
+                <PaperView originData={originData} setOriginData={setOriginData} />
             </BigNav>
         </>
     );
--- src/components/Navigator/Paperview/PaperView.tsx
+++ src/components/Navigator/Paperview/PaperView.tsx
@@ -17,7 +17,7 @@ const Row = styled(motion.div)`
     flex-wrap: wrap;
     gap: 1px;
     margin: 5px 1px 1px 5px;
-    // padding-left: 50px;
+    padding-left: 30px;
     max-height: 150px;
     // grid-template-columns: repeat(16, 1fr);
     // position: absolute;
@@ -59,22 +59,39 @@ const rowVariants = {
     },
 };
 
-const offset = 60;
+function filterImg(originData: SankeyData) {
+    const filteredImg = originData.links.find((paper, i) => {
+        if (paper.color !== 'grayLinkColor' && originData.status[i].paperName === paper.paperName) {
+            console.log(1);
+            return true;
+        } else {
+            console.log(0);
+            return false;
+        }
+    });
+    let findImgPaperName: string = filteredImg ? filteredImg.paperName! : '';
 
-export const PaperView = () => {
-    const [originData, setOriginData] = useState<SankeyData>(fullData);
+    return findImgPaperName;
+}
+
+export const PaperView = ({ originData, setOriginData }: PaperProps) => {
     const [index, setIndex] = useState(0);
     const renderingData: SankeyData = { ...originData };
-
+    const offset = 60;
     useEffect(() => {
-        console.log(renderingData);
+        // console.log(renderingData);
     }, [setOriginData]);
 
-    renderingData.links = renderingData.links.map((links) => {
-        return { ...links };
+    const linkStatus = renderingData.links.filter((links) => {
+        if (links.source >= 100) {
+            return { ...links };
+        } else return null;
     });
+    console.log(linkStatus);
+
     return (
         <>
+            <div style={{ marginLeft: '10px', marginTop: '10px', fontSize: '20px', fontWeight: '650' }}>PaperView</div>
             <Row variants={rowVariants} initial="initial" animate="visible" exit="exit" transition={{ type: 'tween', duration: 1 }} key={index}>
                 {renderingData.status
                     .slice(0)
@@ -82,19 +99,10 @@ export const PaperView = () => {
                     .map((paper) => (
                         <PaperBox layoutId={paper.imgUrl + 'layout'} key={paper.imgUrl} initial="normal" transition={{ type: 'tween' }} bgPhoto={'w60'}>
                             {paper.imgUrl ? <img width="111" height="111" src={`https://i.imgur.com/${paper.imgUrl}`}></img> : null}
+                            {paper.imgUrl ? <div style={{ fontSize: '12px', fontWeight: 'bold', textAlign: 'center' }}>{paper.paperName}</div> : null}
                         </PaperBox>
                     ))}
-                {/* {statusImgSet
-                    .slice(0)
-                    .slice(offset * index, offset * index + offset)
-                    .map((paper) => (
-                        <PaperBox layoutId={paper.imgUrl + 'layout'} key={paper.imgUrl} initial="normal" transition={{ type: 'tween' }} bgPhoto={'w60'}>
-                            {paper.imgUrl ? <img width="111" height="111" src={`https://i.imgur.com/${paper.imgUrl}`}></img> : null}
-                        </PaperBox>
-                    ))} */}
             </Row>
-
-            <div>hi</div>
         </>
     );
 };
