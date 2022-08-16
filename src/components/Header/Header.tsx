548c7f6eed71655695ebab1d07c5989b6500f4b5
hm00081 <hm00081@naver.com>
2022-08-16 14:58:28 +0900

    Header 기능 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,9 @@
 import style from './index.module.scss';
 
 export default function Header() {
-    return <div className={style.navi}>MetaphorViz</div>;
+    return (
+        <a className={style.mainLink} href="/">
+            <div className={style.navi}>MetaphorViz</div>
+        </a>
+    );
 }
