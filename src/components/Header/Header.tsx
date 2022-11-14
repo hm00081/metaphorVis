1ca590074e1d4f7aae3f65ee6c68a0b20e90a4be
문광혁 <moonkwanghyuk@mungwanghyeog-ui-MacBookAir.local>
2022-11-14 15:16:35 +0900

    수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import style from './index.module.scss';
 
 export default function Header() {
     return (
-        <a className={style.mainLink} href="/metaphorViz">
+        <a className={style.mainLink} href="/metaphorVis">
             <div className={style.navi}>MetaphorVis</div>
         </a>
     );
